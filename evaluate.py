9970cdb8985c79a51fc7ff616cf18ef5da9bcd98
calmisential <xinyu_std@163.com>
2019-08-13 15:15:32 +0800

    update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
             tf.config.experimental.set_memory_growth(gpu, True)
 
     # get the original_dataset
-    train_dataset, test_dataset, train_count, test_count = get_datasets()
-    print(train_dataset)
+    train_generator, valid_generator, test_generator, train_num, valid_num, test_num = get_datasets()
+    # print(train_dataset)
     # load the model
     new_model = get_model()
 
     new_model.load_weights(filepath=config.save_model_dir)
     print("----------start evaluating---------")
     # Get the accuracy on the test set
-    loss, acc = new_model.evaluate(test_dataset, steps=test_count // config.BATCH_SIZE)
+    loss, acc = new_model.evaluate_generator(test_generator, steps=test_num // config.BATCH_SIZE)
     print("The accuracy on test set is: {:6.3f}%".format(acc*100))
